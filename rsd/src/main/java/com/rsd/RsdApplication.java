03ff43dd14a6b19c39d3962fee278222fecc3121
Pankaj Chauhan <51152024+Pnkjo7@users.noreply.github.com>
2020-09-14 09:48:25 +0530

    Add files via upload
    maven project

---
+++ rsd/src/main/java/com/rsd/RsdApplication.java
@@ -0,0 +1,13 @@
+package com.rsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsdApplication.class, args);
+	}
+
+}
